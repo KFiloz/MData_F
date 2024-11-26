c7e99e7114a66195efdb69a361d23032cbdf61f5
Kadir <115056694+KFiloz@users.noreply.github.com>
2024-11-26 16:50:10 -0500

    Actualizado codigo

--- app.py
+++ app.py
@@ -21,27 +21,20 @@ dynamodb = boto3.resource(
 )
 table = dynamodb.Table('bd-lorawan')
 
-
 # Función para obtener los últimos 4 datos de DynamoDB
 def get_latest_data(limit=4):
     try:
-        # Escanear la tabla
         response = table.scan()
         data = response['Items']
 
-        # Convertir todos los valores de "timestamp" a un tipo consistente (int)
         for item in data:
             if 'timestamp' in item:
-                # Intentar convertir el valor a int, si no es posible, usar un valor por defecto
                 try:
                     item['timestamp'] = int(item['timestamp'])
                 except ValueError:
-                    item['timestamp'] = 0  # Asignar 0 si no se puede convertir
+                    item['timestamp'] = 0
 
-        # Ordenar los datos por el campo "timestamp"
         data = sorted(data, key=lambda x: x.get('timestamp', 0), reverse=True)
-
-        # Tomar solo los últimos "limit" elementos
         return data[:limit]
     except Exception as e:
         st.error(f"Error al obtener datos de AWS: {e}")
@@ -49,10 +42,8 @@ def get_latest_data(limit=4):
 
 # Función para crear un mapa interactivo
 def create_map(data):
-    # Crear un mapa centrado en una ubicación inicial
     m = folium.Map(location=[10.987103, -74.790072], zoom_start=10)
 
-    # Agregar marcadores al mapa
     for item in data:
         lat = float(item.get('Latitud', 0))
         lon = float(item.get('Longitud', 0))
@@ -74,14 +65,14 @@ def create_map(data):
 
 # Interfaz de Streamlit
 st.title("Mapa de Dispositivos IoT - Card Tracker")
-st.write("Esta aplicación muestra los últimos datos de los dispositivos conectados.")
+st.write("Recarga la página para actualizar los datos más recientes.")
 
-# Obtener y mostrar los últimos datos
-st.subheader("Últimos 4 Datos de DynamoDB")
+# Mostrar los últimos datos y el mapa
+st.subheader("Últimos 4 Datos recibidos")
 latest_data = get_latest_data(limit=4)
-st.write(latest_data)  # Mostrar datos en tabla
+st.write(latest_data)
 
-# Crear y mostrar el mapa con los últimos datos
 st.subheader("Mapa Interactivo")
 mapa = create_map(latest_data)
 st_folium(mapa, width=700, height=500)
+
